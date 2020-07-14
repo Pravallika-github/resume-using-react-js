6d9b588b5d95edc11de5881795dcc76231c3516d
pravallika <pravallikasanapala1@gmail.com>
2020-07-14 22:52:10 +0530

    Updates

---
+++ precache-manifest.810b6903bdbe2637f40dedd3419d6b9a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6b309bc203410800e15ded94d8c1e1",
+    "url": "/github.io/resume-using-react-js/index.html"
+  },
+  {
+    "revision": "7dcf6ddbb566065fe548",
+    "url": "/github.io/resume-using-react-js/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "07bf8f01cd7e1fe6353e",
+    "url": "/github.io/resume-using-react-js/static/css/main.e9286201.chunk.css"
+  },
+  {
+    "revision": "7dcf6ddbb566065fe548",
+    "url": "/github.io/resume-using-react-js/static/js/2.185f7e53.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/github.io/resume-using-react-js/static/js/2.185f7e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bf8f01cd7e1fe6353e",
+    "url": "/github.io/resume-using-react-js/static/js/main.a498462b.chunk.js"
+  },
+  {
+    "revision": "c6bb2604eac2912cc17e",
+    "url": "/github.io/resume-using-react-js/static/js/runtime-main.acc9f781.js"
+  },
+  {
+    "revision": "139c91242899e28f52a9fd3cd8e03f1c",
+    "url": "/github.io/resume-using-react-js/static/media/pravi.139c9124.jpg"
+  },
+  {
+    "revision": "7a0882c78e4cab590ef1dd5aa6bc13fc",
+    "url": "/github.io/resume-using-react-js/static/media/sana.7a0882c7.jpg"
+  }
+]);
